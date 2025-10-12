bc413c4e3848059df0d1b23f53945419df08403a
Ana  Preciado <anapreciado@Anas-MacBook-Pro.local>
2025-10-12 17:10:37 -0500

    POPIP-47: Add module export to asssertions.json

--- includes/assertions.js
+++ includes/assertions.js
@@ -1,4 +1,8 @@
 // includes/docs.js
 
 // Row Conditions
-const table_successfully_updated = `DATE(processed_at) = DATE(CURRENT_TIMESTAMP()).`;
+const table_successfully_updated = `"DATE(processed_at) = DATE(CURRENT_TIMESTAMP())"`;
+
+module.exports = {
+   table_successfully_updated
+}
