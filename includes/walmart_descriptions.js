a0561b542d9787055ef8b62cf20bf355c31c6b96
Ana  Preciado <anapreciado@Anas-MacBook-Pro.local>
2025-11-17 15:31:39 -0500

    POPIP-89: Create calendar features.

--- includes/walmart_descriptions.js
+++ includes/walmart_descriptions.js
@@ -9,10 +9,10 @@ const ctx_cat_id = `Category of the item.`;
 const ctx_dept_id = `Department of the Item (can be thought of a sub-category).`;
 const ctx_item_id = `The timestamp when the row was processed.`;
 const tgt_monthly_sales = `TGT monthly sales for observation.`;
-const tgt_monthly_sales_lead_1_months = `TGT monthly sales for observation.`;
-const tgt_monthly_sales_lead_2_months = `TGT monthly sales for observation.`;
-const tgt_monthly_sales_lead_3_months = `TGT monthly sales for observation.`;
-const tgt_monthly_sales_lead_4_months = `TGT monthly sales for observation.`;
+const tgt_monthly_sales_lead_1_months = `TGT monthly sales lead 1 month for observation.`;
+const tgt_monthly_sales_lead_2_months = `TGT monthly sales lead 2 months for observation.`;
+const tgt_monthly_sales_lead_3_months = `TGT monthly sales lead 3 months for observation.`;
+const tgt_monthly_sales_lead_4_months = `TGT monthly sales lead 4 months for observation.`;
 
 module.exports = {
     ctx_date_month,
